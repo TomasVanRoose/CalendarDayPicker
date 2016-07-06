f7702725a574fdec9b0b71578a6c33c868b5c4dd
Tomas Van Roose <Tomas.VanRoose@UGent.be>
2016-07-06 14:07:16 +0200

    Rename without NS, add firstdayofmonth func

--- CalendarDayPicker/DateExtension.swift
+++ CalendarDayPicker/DateExtension.swift
@@ -8,10 +8,30 @@
 
 import Foundation
 
-extension NSDate {
+internal extension DateComponents {
+    mutating func to12pm() {
+        self.hour = 12
+        self.minute = 0
+        self.second = 0
+    }
+}
+
+extension Date {
+
     
     static func weekdays() -> [String] {
         return [ "Ma", "Di", "Wo", "Do", "Vr", "Za", "Zo"]
+    }
+    
+    
+    func firstDayOfMonth() -> NSDate? {
+        guard
+            let cal: Calendar = Calendar.current(),
+            var comp: DateComponents = cal.components([.year, .month], from: self) else {return nil}
+        
+        comp.to12pm()
+        
+        return cal.date(from: comp)!
     }
     
     
