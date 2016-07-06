c05f62cdf1d446edfee60755c41152d2ded1a3c3
Tomas Van Roose <Tomas.VanRoose@UGent.be>
2016-07-06 13:30:52 +0200

    Add NSDate extension
    Function weekdays returns short days of the week, currently only in dutch

---
+++ CalendarDayPicker/DateExtension.swift
@@ -0,0 +1,18 @@
+//
+//  DateExtension.swift
+//  CalendarDayPicker
+//
+//  Created by Tomas Van Roose on 05/07/16.
+//  Copyright © 2016 Tomas Van Roose. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate {
+    
+    static func weekdays() -> [String] {
+        return [ "Ma", "Di", "Wo", "Do", "Vr", "Za", "Zo"]
+    }
+    
+    
+}
